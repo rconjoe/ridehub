5570006477fde6286e28e3f66be3c39a8e1aab65
rconjoe <joe@rconjoe.com>
2022-02-09 19:17:26 -0800

    init Inter font

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter'
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+  })
+  if (!fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <Text> Loading... </Text>
+      </View>
+    )
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'Inter_900Black' }}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,6 +23,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    fontSize: 40,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
